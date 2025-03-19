3e38e156c1f29069f0ae3755a045a9f0bd55c960
Felipe Olavo <felipegames1307@gmail.com>
2025-03-19 10:22:45 -0300

    Update and rename program to calculate the area of ​​a triangle..c to 7-program to calculate the area of ​​a triangle..c

--- 7-program to calculate the area of ​​a triangle..c
+++ 7-program to calculate the area of ​​a triangle..c
@@ -4,7 +4,7 @@ int main(){
 float area, base, altura;
 printf("Digite o valor da base em metros: ");
 scanf("%f",&base);
-/* & ''endereço'' serve para mostrar em qual local sera armazenada*/
+/* & ''endereÃ§o'' serve para mostrar em qual local sera armazenada*/
 printf("Digite o valor da altura em metros: ");
 scanf("%f",&altura);
 area= (base * altura)/2;
